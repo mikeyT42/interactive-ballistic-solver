01769621d0b0be1046307ab7fd211bfe96a1bb62
Michael Torres <mrmojorisen0@gmail.com>
2026-09-17 22:52:22 -0400

    I am starting the refactor so that I can gain understanding. I already moved the constants and the shot result object out.

---
+++ ShotResult.jl
@@ -0,0 +1,7 @@
+struct ShotResult
+    isValid::Bool
+    turretYaw°::Float64
+    hoodPitch°::Float64
+    v_flywheel::Float64
+    simError::Float64
+end
--- main.jl
+++ main.jl
@@ -1,5 +1,7 @@
 using GLMakie
 
+include("constants.jl")
+include("ShotResult.jl")
 
 # ══════════════════════════════════════════════════════════════════════════════
 #  Greek Letter Names
@@ -9,37 +11,6 @@ using GLMakie
 # ρ : Lowercase Rho
 # ϵ : Lowercase Epsilon
 # θ : Lowercase Theta
-
-# ══════════════════════════════════════════════════════════════════════════════
-#  Physical Constants
-# ══════════════════════════════════════════════════════════════════════════════
-const g   = 9.81                       # gravitational acceleration     [m/s²]
-const M   = 0.216                      # ball mass                        [kg]
-const R   = 0.075                      # ball radius                      [m]
-const A   = π * R^2                    # cross-sectional area             [m²]
-const ρ   = 1.199                      # air density (20°C,101 kPa,50%RH)[kg/m³]
-const C_D = 0.47                       # drag coefficient
-const C_L = 0.031                      # lift (Magnus) coefficient
-
-# ══════════════════════════════════════════════════════════════════════════════
-#  Solver Tuning
-# ══════════════════════════════════════════════════════════════════════════════
-const N_SECANT = 5                     # max secant iterations
-const Δt₀     = 0.01                   # integration time-step            [s]
-const t_max   = 5.0                    # simulation time ceiling          [s]
-const v_max   = 35.0                   # flywheel speed ceiling           [m/s]
-const ε_h     = 0.02                   # height-error tolerance           [m]
-
-# ══════════════════════════════════════════════════════════════════════════════
-#  Data Structs
-# ══════════════════════════════════════════════════════════════════════════════
-struct ShotResult
-    isValid::Bool
-    turretYaw°::Float64
-    hoodPitch°::Float64
-    v_flywheel::Float64
-    simError::Float64
-end
 
 # ══════════════════════════════════════════════════════════════════════════════
 #  Main Ballistic Solver
